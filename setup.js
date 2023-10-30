91ca06bd6a5f67940308e572fcc0782f78fb9e1f
ITSCDT001 <148861278+ITSCDT001@users.noreply.github.com>
2023-10-30 16:56:42 +0000

    Delete setup.js

--- setup.js
+++
@@ -1,6 +0,0 @@
-export function setup() {
-    let background = "https://images.unsplash.com/photo-1556139930-c23fa4a4f934?auto=format&fit=crop&q=80&w=3570&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    document.body.style.backgroundImage = `url(${background})`;
-    document.body.style.backgroundSize = "cover";
-
-}
