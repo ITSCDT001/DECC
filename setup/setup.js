bf337effdef01b0a2bbeddd5125f70332c2fc608
ITSCDT001 <148861278+ITSCDT001@users.noreply.github.com>
2023-11-09 10:28:15 +0000

    Delete setup directory

--- setup/setup.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-    let background = "../src/background4.jpg";
-    document.body.style.backgroundImage = `url(${background})`;
-    document.body.style.backgroundSize = "cover";
-}
-
-setup();
