e2db71d9f7498e06befeae492d26be4f06b5a8f4
ITSCDT001 <148861278+ITSCDT001@users.noreply.github.com>
2023-11-09 10:29:11 +0000

    Delete deePages directory

--- deePages/dee01.js
+++
@@ -1,7 +0,0 @@
-// ----------------- DEE01 ---------------------
-
-import { animateInTimeline } from "/utilities/timeline.js";
-
-animateInTimeline("card-1", "fadeIn", 0);
-animateInTimeline("card-2", "fadeIn", 1);
-animateInTimeline("card-2", "fadeOut", 2);
