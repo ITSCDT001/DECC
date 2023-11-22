bf566444a559d720d699ef1e8fca5fc6c69442ca
ITSCDT001 <148861278+ITSCDT001@users.noreply.github.com>
2023-11-22 17:47:06 +0000

    Delete Concept directory

--- Concept/AmazonDistribution.js
+++
@@ -1,7 +0,0 @@
-import { animateInTimeline } from "/utilities/timeline.js";
-
-animateInTimeline("slide1-img", "fadeOut", 0, true);
-
-animateInTimeline("black-screen", "fadeOut", 0, true);
-
-document.querySelector("main").style.opacity = 1;
