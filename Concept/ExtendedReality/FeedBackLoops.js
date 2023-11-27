c8ab9136652ad3e89e01f85bb8ed50d19645bb75
james-hickmott <117409902+james-hickmott@users.noreply.github.com>
2023-11-27 09:49:24 +0000

    Add files via upload

--- Concept/ExtendedReality/FeedBackLoops.js
+++ Concept/ExtendedReality/FeedBackLoops.js
@@ -4,5 +4,7 @@ animateInTimeline("slide1-container-id", "fadeOut", 1, true);
 animateInTimeline("slide2-container-id", "fadeIn");
 animateInTimeline("slide2-container-id", "fadeOut", 1);
 animateInTimeline("slide3-container-id", "fadeIn");
+animateInTimeline("slide3-container-id", "fadeOut", 1);
+animateInTimeline("slide4-container-id", "fadeIn");
 
 document.querySelector("main").style.opacity = 1;
